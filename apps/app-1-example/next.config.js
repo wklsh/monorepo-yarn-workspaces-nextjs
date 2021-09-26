5fac704b00d28de606297426cbcc0a77cfdd0dbf
WKLSH <wklsh@hotmail.sg>
2021-09-27 02:59:04 +0800

    feat: Config tweaks to support various build conditions

--- apps/app-1-example/next.config.js
+++ apps/app-1-example/next.config.js
@@ -1,13 +1,29 @@
 const withTM = require("next-transpile-modules")(["common"]); // pass the modules you would like to see transpiled
 const withImages = require("next-images");
 
+if (process.env.IS_EXPORTING) {
+  console.log(
+    "\n",
+    "\n",
+    "----------------------------------------------------------------------------",
+    "\n",
+    "STATIC EXPORTING:",
+    "\n",
+    "MAKE SURE THAT YOU DO NOT HAVE ANY <IMAGE /> ELEMENTS. THESE DO NOT WORK!",
+    "\n",
+    "----------------------------------------------------------------------------",
+    "\n",
+    "\n"
+  );
+}
+
 module.exports = withTM(
   withImages({
     assetPrefix: ".",
     distDir: `../../build-dist/${process.env.npm_package_name}`,
     images: {
-      loader: "imgix",
-      path: "/",
+      loader: process.env.IS_EXPORTING ? "imgix" : null,
+      path: process.env.IS_EXPORTING ? "/" : null,
     },
   })
 );
